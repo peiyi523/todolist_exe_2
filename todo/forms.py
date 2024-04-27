ca5ed9c1400ab78ee17aff1873e4c88c35fd93ea
peiyi <peiyi523@gmail.com>
2024-04-27 17:26:33 +0800

    完成create_todo建立功能

---
+++ todo/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Todo
+
+
+class TodoForm(ModelForm):
+    # 表單綁定資料模型
+    class Meta:
+        model = Todo
+        # fields = "__all__"  # 代表全欄位都要
+        fields = ["title", "text", "important"]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
+
+
+def create_todo(request):
+    # GET
+    message = ""
+    form = TodoForm()
+    # POST
+    if request.method == "POST":
+        try:
+            # print(request.POST)
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            message = "建立事項成功！"
+            return redirect("todolist")
+        except Exception as e:
+            print(e)
+            message = "建立事項失敗..."
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
 
 
 def todo(request):
